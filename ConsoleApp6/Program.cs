f3fb687bf0ecb96fd3b803c56849d28d6fd11eb4
RFD <rezafeizi214@gmail.com>
2024-05-28 20:58:46 +0330

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿int a =int.Parse(Console.ReadLine());
+
+switch(a)
+{
+    case 1:
+        {
+            Console.WriteLine("one");
+            break;
+        }
+        case 2:
+        {
+            break;
+        }
+}
